6f9a22c93490338b72fbbb0614e370921da9c2c5
Clement <122646162+poytaytoy@users.noreply.github.com>
2026-08-30 21:40:17 -0400

    Add: products-of-array-discluding-self - submission-7

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-7.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        
+        int zero_index = -1;
+        int accum = 1; 
+
+        for (int i = 0; i < nums.size(); i++){
+            if (nums[i] == 0 && zero_index == -1){
+                zero_index = i;  
+                continue;
+            }
+
+            accum *= nums[i];
+        } 
+
+        if (zero_index != -1){
+            vector<int> output(nums.size(), 0);
+
+            output[zero_index] = accum;
+
+            return output;  
+        }
+
+        vector<int> output; 
+
+        for (int i = 0; i < nums.size(); i++){
+            output.emplace_back(accum / nums[i]);
+        } 
+
+        return std::move(output); 
+
+    }
+};

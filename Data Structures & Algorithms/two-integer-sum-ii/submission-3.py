239091d4a2cc386807392cf41846ad8175711d7b
Clement <122646162+poytaytoy@users.noreply.github.com>
2026-09-02 14:28:00 -0400

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        index_cap = len(numbers)
+        
+        pair = [-1001, -1001]
+
+        for i in range(0, index_cap):
+            if (i >= index_cap):
+                break
+            
+            end_loop = False 
+        
+            for j in range(index_cap - 1, i, -1):
+                bum = numbers[j] + numbers[i]
+
+                if bum > target: 
+                    index_cap = j
+
+                if bum < target: 
+                    break
+
+                if bum == target: 
+                    pair = [i + 1, j + 1]
+                    end_loop = True 
+                    break; 
+            
+            if end_loop: 
+                break
+
+        return pair 
